cc350592486a53c846a476ce37bdb1d8c6487def
fabricio raphael <fabricioraphael@pop-os.localdomain>
2019-04-29 19:37:17 -0300

    removing react-native-svg call

--- src/stacked-bar-chart/stacked-bar-grouped.js
+++ src/stacked-bar-chart/stacked-bar-grouped.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { View } from 'react-native'
 import PropTypes from 'prop-types'
-import Svg from 'react-native-svg'
+import { Svg } from 'expo'
 import * as array from 'd3-array'
 import * as scale from 'd3-scale'
 import * as shape from 'd3-shape'
